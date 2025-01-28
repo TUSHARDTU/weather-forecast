8a04abd44f195a0d459c0f5eada7746dd2ab7aaf
Tushar DTU <trohilla9992145124@gmail.com>
2025-01-28 12:44:48 +0530

    first commit

---
+++ 02/patterns/weather.app/script.js
@@ -0,0 +1,26 @@
+const button = document.getElementById("search-button");
+const input = document.getElementById('city-input');
+
+const cityName = document.getElementById('city-name');
+const cityTime = document.getElementById('city-time');
+const cityTemp = document.getElementById('city-temp');
+
+
+ async function getdata(cityName){
+   const promise= await fetch(
+     `https://api.weatherapi.com/v1/current.json?key=230235bd2aac4a76bc650641252801&q=${cityName}&aqi=yes`
+
+    );
+    return await promise.json();
+
+}
+
+button.addEventListener("click", async () => {
+   const value = input.value;
+  const result = await getdata(value);
+  cityName.innerText = `${result.location.name}, ${result.location.region} - ${result.location.country}`;
+  cityTime.innerText = result.location.localtime;
+  cityTemp,innerText = result.current.temp_c;
+
+});
+
